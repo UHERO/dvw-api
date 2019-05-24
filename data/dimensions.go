976326b8908ec85e8807fd4ab300e285b54ceaf8
dji <David.Iannucci@hawaii.edu>
2019-05-24 11:51:27 -1000

    wip

--- data/dimensions.go
+++ data/dimensions.go
@@ -1,23 +1,43 @@
 package data
 
-import "fmt"
+import (
+	"fmt"
+)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 func GetDimensionAll(dim string, mod string) (dimList []Dimension, err error) {
-	var xtraCols string
+	xtraCols := ", null, null"  // No unit or decimals for all tables other than indicators
 	if dim == "indicators" {
 		xtraCols = ", t.unit, t.decimal"
 	}
 	//language=MySQL
 	var query = fmt.Sprintf(`
-          select t.module, t.handle, t.nameP, t.nameW, t.nameT, t.header, p.handle as parent, t.level, t.order%s
+          select t.module, t.handle, t.nameP, t.nameW, t.nameT, t.header, p.handle as parent, t.level, t.order %s
           from %s t left join %s p on p.id = t.parent_id
           where t.module = ?`, xtraCols, dim, dim)
-	rows, err := Db.Query(query, mod)
+	results, err := Db.Query(query, mod)
 	if err != nil {
 		return
 	}
-	for rows.Next() {
+	for results.Next() {
+		scanDim := Dimension{}
+		err = results.Scan(
+			&scanDim.Module,
+			&scanDim.Handle,
+			&scanDim.NameP,
+			&scanDim.NameW,
+			&scanDim.NameT,
+			&scanDim.Header,
+			&scanDim.Parent,
+			&scanDim.Level,
+			&scanDim.Order,
+			&scanDim.Unit,
+			&scanDim.Decimal,
+		)
+		if err != nil {
+			return
+		}
+		dimList = append(dimList, scanDim)
 	}
 	return
 }
